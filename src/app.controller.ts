24f8421fe60f628d87b50b993429ce7ece6c622d
AWKOmcansell <awko.m.cansell@gmail.com>
2022-10-04 16:01:40 +0200

    ok pipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UpperPipe } from './upper.pipe';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':name')
+  @UsePipes(UpperPipe)
+  getHello(@Param('name') name: string): string {
+    return this.appService.getHello(name);
+  }
+
+  @Post()
+  @UsePipes(UpperPipe)
+  createMessage(@Body() message) {
+    return message;
   }
 }
